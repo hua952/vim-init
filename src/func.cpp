48c9adbae382355d626749b2be43b3b41bee8a8f
hua952 <hua_0_@hotmail.com>
2023-04-07 18:40:04 +0800

    完善小程序

--- src/func.cpp
+++ src/func.cpp
@@ -14,7 +14,7 @@ int main (int nArg, char* argS[])
 	int nT = 4;
 	std::string strLine;
 	getline (std::cin, strLine);
-	auto szR = R"--((\w+.+)(\b\w[\w\d]*\s*\(.*\))\s*(\w*)(;*))--";
+	auto szR = R"--((\s*)(\w+.+)(\b\w[\w\d]*\s*\(.*\))\s*(\w*)(;*))--";
 	/*
 	auto& rE = std::cerr;
 	rE<<" szR = "<<szR<<std::endl;
@@ -22,12 +22,12 @@ int main (int nArg, char* argS[])
 	std::regex r(szR);
 	std::stringstream ss;
 	if (className.empty ()) {
-		ss<<"$1$2$3";
+		ss<<"$2$3$4";
 	} else {
-		ss<<"$1 "<<className<<":: $2$3";
+		ss<<"$2 "<<className<<":: $3$4";
 	}
 	std::string funH = std::regex_replace(strLine, r, ss.str().c_str());
-	std::string retType = std::regex_replace(strLine, r, "$1");
+	std::string retType = std::regex_replace(strLine, r, "$2");
 	auto& cOut = std::cout;
 	std::string strSpace;
 	cOut<<strSpace<<funH<<std::endl
